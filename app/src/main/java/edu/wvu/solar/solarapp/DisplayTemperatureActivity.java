d1c6825eb0b6976802daa6ba75c26416a76aaff3
Timmy <timjimski@gmail.com>
2015-09-30 16:39:42 -0400

    Added graphing

--- app/src/main/java/edu/wvu/solar/solarapp/DisplayTemperatureActivity.java
+++ app/src/main/java/edu/wvu/solar/solarapp/DisplayTemperatureActivity.java
@@ -1,33 +1,52 @@
 package edu.wvu.solar.solarapp;
 
+import android.app.ActionBar;
 import android.app.Activity;
-import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 
+import org.apache.http.util.ExceptionUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 
 public class DisplayTemperatureActivity extends Activity {
 
-    public static final String SERVER_URL = "http://192.168.0.104";
+    //public static final String SERVER_URL = "http://192.168.0.102:8081";
+    //public static final String SERVER_URL = "http://localhost:8081";
+    public static final String SERVER_URL = "http://10.255.94.7:8081";
     public static final String DEBUG_TAG = "SolarAppDisplayActivity";
+
+    LinearLayout baseLayout;
+    LineChart chart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_temperature);
+        baseLayout = (LinearLayout) findViewById(R.id.baseLayout);
+        chart = new LineChart(this);
+        chart.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+        baseLayout.addView(chart);
     }
 
     @Override
@@ -43,8 +62,60 @@ public class DisplayTemperatureActivity extends Activity {
         String startTime = "20150901T161956-0400";
         String endTime = "20150901T162036-0400";
         String sensorName = "indoor1";
-        RetrieveDataTask task = new RetrieveDataTask();
-        task.doInBackground(sensorName, startTime, endTime);
+        showHistory(startTime, endTime, sensorName, chart);
+
+    }
+
+    private void showHistory(String startTime, String endTime, String sensorName, LineChart chart){
+        String url = SERVER_URL + "/getdata?startTime=" + startTime + "&endTime=" + endTime + "&sensorName=" + sensorName;
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new JsonResponse(chart), new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(DEBUG_TAG, "ERROR! ", error);
+            }
+        });
+        Volley.newRequestQueue(this).add(request);
+    }
+
+    private class JsonResponse implements Response.Listener<JSONArray>{
+
+        private LineChart chart;
+
+        public JsonResponse(LineChart chart){
+            this.chart = chart;
+        }
+
+        @Override
+        public void onResponse(JSONArray response) {
+            Log.i(DEBUG_TAG, "Response! " + response.toString());
+            ArrayList<String> xLabels = new ArrayList<String>();
+            ArrayList<Entry> tempEntries = new ArrayList<Entry>();
+            ArrayList<Entry> humidEntries = new ArrayList<Entry>();
+            for(int i = 0; i < response.length(); i++){
+                try {
+                    //Log.i(DEBUG_TAG, response.getJSONObject(i).toString());
+                    JSONObject obj = response.getJSONObject(i);
+                    LogEntry entry = new LogEntry(obj.getDouble("temp"), obj.getDouble("humidity"), obj.getString("time"));
+                    xLabels.add(entry.getTimeAsDateTime().toString("MM/dd, hh:mm aa"));
+                    tempEntries.add(new Entry((float)entry.getTemp(), i));
+                    humidEntries.add(new Entry((float) entry.getHumidity(), i));
+
+                    Log.i(DEBUG_TAG, entry.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            LineDataSet tempSet = new LineDataSet(tempEntries, "Temperature (f)");
+            LineDataSet humidSet = new LineDataSet(humidEntries, "Humidity (%)");
+            ArrayList<LineDataSet> sets = new ArrayList<LineDataSet>();
+            sets.add(tempSet);
+            sets.add(humidSet);
+            LineData data = new LineData(xLabels, sets);
+            chart.setData(data);
+            chart.invalidate();
+
+        }
     }
 
     @Override
@@ -60,47 +131,5 @@ public class DisplayTemperatureActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public class RetrieveDataTask extends AsyncTask<String, Void, LinkedList<LogEntry>> {
-
-        @Override
-        protected LinkedList<LogEntry> doInBackground(String... params) {
-            String sensorName = params[0];
-            String startTime = params[1];
-            String endTime = params[2];
-            try {
-                /*HttpResponse<JsonNode> response = Unirest.get(SERVER_URL + "/getdata").queryString("startTime", startTime)
-                        .queryString("endTime", endTime).queryString("sensorName", sensorName).asJson();
-                LinkedList<LogEntry> list = new LinkedList<LogEntry>();
-                JSONArray array = response.getBody().getArray();
-                for(int i = 0; i < array.length(); i++){
-                    JSONObject obj = null;
-                    try {
-                        obj = array.getJSONObject(i);
-                        list.add(new LogEntry(obj.getDouble("temp"), obj.getDouble("humidity"), obj.getString("time")));
-                    }catch(JSONException e){
-                        Log.e(DEBUG_TAG, "Error processing JSON. Object: " + obj.toString());
-                        Log.e(DEBUG_TAG, "Exception: " + e.getMessage());
-                    }
-                }
-                return list;*/
-
-
-            } catch(Exception e){
-                /*catch (UnirestException e) {
-                Log.e(DEBUG_TAG, e.getMessage());
-                return new LinkedList<LogEntry>();
-                }*/
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(LinkedList<LogEntry> result){
-            for(LogEntry entry : result){
-                Log.i(DEBUG_TAG, entry.toString());
-            }
-        }
     }
 }
